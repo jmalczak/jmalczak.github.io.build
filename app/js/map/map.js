ca66a4242311e550a3a38f75d7ea98ed43a51bc2
Jakub Malczak <jmalczak@gmail.com>
2015-09-13 22:29:29 +0800

    Added knockout

--- app/js/map/map.js
+++ app/js/map/map.js
@@ -1,12 +1,13 @@
 requirejs(['main'], function(common) {
-    require(['jquery', 'mapbox', 'map/map-markers'], function($, L, M) {
+    require(['jquery', 'mapbox', 'map/map-markers', 'knockout'], function($, L, M, ko) {
+
         var Map = function() {
             var self = this;
 
             self.settings = {
                 accessToken: 'pk.eyJ1IjoibWFsY3p1IiwiYSI6IjYwNWI0ZjI3YmM4OTA1ZGU1ODMwZTJiNGFmZDA1Nzc2In0.IIXH9EyewmF5-_1iEgsLOA',
-    mapName: 'mapbox.light',
-    containerName: 'map'
+                mapName: 'mapbox.light',
+                containerName: 'map'
             };
 
             self.init = function() {
@@ -15,17 +16,23 @@ requirejs(['main'], function(common) {
 
             self.initEvents = function() {
                 $(document).ready(function () {
-                    L.mapbox.accessToken = self.settings.accessToken;
-                    self.map = L.mapbox.map(
-                        self.settings.containerName, 
-                        self.settings.mapName, { 
-                            zoomControl: false,
-                            scrollWheelZoom: false,
-                            attributionControl: {compact: true}
-                        }).setView([40,25], 3);
-
-                    self.addMarkers();
+                    ko.applyBindings(M);
+                    self.configureMapBox();
                 });
+            };
+
+            self.configureMapBox = function() {
+                L.mapbox.accessToken = self.settings.accessToken;
+                
+                self.map = L.mapbox.map(
+                    self.settings.containerName, 
+                    self.settings.mapName, { 
+                        zoomControl: false,
+                        scrollWheelZoom: false,
+                        attributionControl: {compact: true}
+                    }).setView([40,25], 3);
+
+                self.addMarkers();
             };
 
             self.addMarkers = function() {
--- app/js/shared/main.js
+++ app/js/shared/main.js
@@ -5,6 +5,7 @@ requirejs.config({
         bootstrap: '../_bower_components/bootstrap/dist/js/bootstrap',
         ekkolightbox: '../_bower_components/ekko-lightbox/dist/ekko-lightbox',
         mapbox: '../_bower_components/mapbox.js/mapbox',
+        knockout: '../_bower_components/knockout/dist/knockout',
     },
     shim: {
         bootstrap: {
@@ -17,7 +18,11 @@ requirejs.config({
         },
         mapbox : {
             exports: 'L'
+        },
+        knockout : {
+            exports: 'ko'
         }
+
     }
 });
 
