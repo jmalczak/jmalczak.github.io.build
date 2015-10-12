3683031f1d6ae15c6c8cadc362df8e269343f779
Jakub Malczak <jmalczak@gmail.com>
2015-10-12 12:21:35 +0900

    replaced double to single quotes

--- app/js/map/map-markers.js
+++ app/js/map/map-markers.js
@@ -3,229 +3,229 @@ define(function() {
         var self = this;
 
         self.continents = [{
-            name: "Europe",
-            img: "europe.png",
+            name: 'Europe',
+            img: 'europe.png',
             countries: [{ 
-                    name: "Poland",
+                    name: 'Poland',
                     lat: 52.2167,
                     lon: 21.0333,
-                    description: "I live in third largest city in Poland called Lodz",
+                    description: 'I live in third largest city in Poland called Lodz',
                 }, { 
-                    name: "Germany",
+                    name: 'Germany',
                     lat: 51.75925,
                     lon: 19.45598,
-                    description: "Just Berlin for a weekend",
+                    description: 'Just Berlin for a weekend',
                 }, {
-                    name: "Austria",
+                    name: 'Austria',
                     lat: 48.2000,
                     lon: 16.3500,
-                    description: "Vienna for a weekend",
+                    description: 'Vienna for a weekend',
                 }, {
-                    name: "Hungary",
+                    name: 'Hungary',
                     lat: 47.4333,
                     lon: 19.2500,
-                    description: "Budapest during Balkan trip",
+                    description: 'Budapest during Balkan trip',
                 }, {
-                    name: "Serbia",
+                    name: 'Serbia',
                     lat: 44.8000,
                     lon: 20.4667,
-                    description: "Belgrade during Balkan trip",
+                    description: 'Belgrade during Balkan trip',
                 }, {
-                    name: "Montenegro",
+                    name: 'Montenegro',
                     lat: 42.7833,
                     lon: 19.4667,
-                    description: "Zablijak, Podgorica, Kotor during Balkan trip",
+                    description: 'Zablijak, Podgorica, Kotor during Balkan trip',
                 }, {
-                    name: "Croatia",
+                    name: 'Croatia',
                     lat: 45.8000,
                     lon: 16.0000,
-                    description: "Dubrownik, Split, Plitvice, Zagreb during Balkan trip",
+                    description: 'Dubrownik, Split, Plitvice, Zagreb during Balkan trip',
                 }, {
-                    name: "Greece",
+                    name: 'Greece',
                     lat: 39.0000,
                     lon: 22.0000,
-                    description: "Crete and Santorini islands",
+                    description: 'Crete and Santorini islands',
                  }
                 , {
-                    name: "Italy",
+                    name: 'Italy',
                     lat: 41.9000,
                     lon: 12.4833,
-                    description: "Turin, Genoa, Venice, Rome during two different trips",
+                    description: 'Turin, Genoa, Venice, Rome during two different trips',
                 }
                 , {
-                    name: "Switzerland",
+                    name: 'Switzerland',
                     lat: 46.8333,
                     lon: 8.3333,
-                    description: "Zermatt, Matternhorn climb trip",
+                    description: 'Zermatt, Matternhorn climb trip',
                 }
                 , {
-                    name: "France",
+                    name: 'France',
                     lat: 47.0000,
                     lon: 2.0000,
-                    description: "Just Paris",
+                    description: 'Just Paris',
                 }
                 , {
-                    name: "Spain",
+                    name: 'Spain',
                     lat: 40.41678,
                     lon: -3.70379,
-                    description: "Barcelona, Valencia, Madrid, Zaragoza during Spain road trip",
+                    description: 'Barcelona, Valencia, Madrid, Zaragoza during Spain road trip',
                 }
                 , {
-                    name: "Andorra",
+                    name: 'Andorra',
                     lat: 42.50628,
                     lon: 1.52180,
-                    description: "Andorra la Vella for some tax free goodies",
+                    description: 'Andorra la Vella for some tax free goodies',
                 }
                 , {
-                    name: "England",
+                    name: 'England',
                     lat: 52.35552,
                     lon: -1.17432,
-                    description: "Studying and working abroad (Coventry, Northampton) for a while",
+                    description: 'Studying and working abroad (Coventry, Northampton) for a while',
                 }
                 , {
-                    name: "Ireland",
+                    name: 'Ireland',
                     lat: 53.41291,
                     lon: -8.24389,
-                    description: "Galway, Dublin, Tralee and couple of smaller places",
+                    description: 'Galway, Dublin, Tralee and couple of smaller places',
                 }
                 , {
-                    name: "Sweden",
+                    name: 'Sweden',
                     lat: 60.12816,
                     lon: 18.64350,
-                    description: "Stockholm for a couple of days",
+                    description: 'Stockholm for a couple of days',
                 }
                 , {
-                    name: "Finland",
+                    name: 'Finland',
                     lat: 61.92411,
                     lon: 25.74815,
-                    description: "Helsinki one day trip from Sweden",
+                    description: 'Helsinki one day trip from Sweden',
                 }
                 , {
-                    name: "Lithuania",
+                    name: 'Lithuania',
                     lat: 55.16944,
                     lon: 23.88127,
-                    description: "Vilnius",
+                    description: 'Vilnius',
                 }
                 , {
-                    name: "Latvia",
+                    name: 'Latvia',
                     lat: 56.87964,
                     lon: 24.60319,
-                    description: "Riga",
+                    description: 'Riga',
                 }
                 , {
-                    name: "Estonia",
+                    name: 'Estonia',
                     lat: 58.59527,
                     lon: 25.01361,
-                    description: "Tallin",
+                    description: 'Tallin',
                 }]
             }, {
-            name: "Asia",
-            img: "asia.png",
+            name: 'Asia',
+            img: 'asia.png',
             countries: [{ 
-                    name: "Nepal",
+                    name: 'Nepal',
                     lat: 28.39486,
                     lon: 84.12401,
-                    description: "Kathmandu, Pokhara and all small places on the Annapurna Circuit trek"
+                    description: 'Kathmandu, Pokhara and all small places on the Annapurna Circuit trek'
                 } 
                 , {
-                    name: "India",
+                    name: 'India',
                     lat: 20.59368,
                     lon: 78.96288,
-                    description: "Gorakhpur, New Delhi, Agra, Jaipur, Udaipur, Mumbai, Goa, Kerala" +
-                                 " all the way down to Kanyakumari"
+                    description: 'Gorakhpur, New Delhi, Agra, Jaipur, Udaipur, Mumbai, Goa, Kerala' +
+                                 ' all the way down to Kanyakumari'
                 }
                 , {
-                    name: "Sri Lanka",
+                    name: 'Sri Lanka',
                     lat: 7.87305,
                     lon: 80.77180,
-                    description: "Colombo, Kandy, Ella, Tissa, Arugam Bay, Trincomalee, Anuradhapura" +
-                                 " Dambulla and Negombo"
+                    description: 'Colombo, Kandy, Ella, Tissa, Arugam Bay, Trincomalee, Anuradhapura' +
+                                 ' Dambulla and Negombo'
                 }
                 , {
-                    name: "Indonesia",
+                    name: 'Indonesia',
                     lat: -0.78927,
                     lon: 113.92133,
-                    description: "Bandung, Pangandaran, Yogyakarta, Bromo, Kawah Ijen" +
-                                 " Gilimanuk, Ubud, Denpasar, Kuta, Lovina, Gili Air, Kuta Lombok"
+                    description: 'Bandung, Pangandaran, Yogyakarta, Bromo, Kawah Ijen' +
+                                 ' Gilimanuk, Ubud, Denpasar, Kuta, Lovina, Gili Air, Kuta Lombok'
                 }
                 , {
-                    name: "Malaysia",
+                    name: 'Malaysia',
                     lat: 4.21048,
                     lon: 101.97577,
-                    description: "Kuala Lumpur"
+                    description: 'Kuala Lumpur'
                 }
                 , {
-                    name: "Singapore",
+                    name: 'Singapore',
                     lat: 1.35208,
                     lon: 103.81984,
-                    description: "Singapore"
+                    description: 'Singapore'
                 }
                 , {
-                    name: "Thailand",
+                    name: 'Thailand',
                     lat: 15.87003,
                     lon: 100.99254,
-                    description: "Bangkok, Krabi, Ko Phi Phi, Chiang Mai"
+                    description: 'Bangkok, Krabi, Ko Phi Phi, Chiang Mai'
                 }
                 , {
-                    name: "Laos",
+                    name: 'Laos',
                     lat: 19.85627,
                     lon: 102.49550,
-                    description: "Vientiane, Vang Vieng, Luang Prabang and all the way north to China"
+                    description: 'Vientiane, Vang Vieng, Luang Prabang and all the way north to China'
                 }
                 , {
-                    name: "Vietnam",
+                    name: 'Vietnam',
                     lat: 14.05832,
                     lon: 108.27720,
-                    description: "Hanoi, Ha-Long Bay, Hoi An, Ho Chi Minh"
+                    description: 'Hanoi, Ha-Long Bay, Hoi An, Ho Chi Minh'
                 }
                 , {
-                    name: "Cambodia",
+                    name: 'Cambodia',
                     lat: 12.56568,
                     lon: 104.99096,
-                    description: "Phnom Penh, Siem Reap to see Angkor Wat"
+                    description: 'Phnom Penh, Siem Reap to see Angkor Wat'
                 }
                 , {
-                    name: "Myanmar",
+                    name: 'Myanmar',
                     lat: 21.91397,
                     lon: 95.95622,
-                    description: "Yangon, Bagan, Mandalay, Kalaw"
+                    description: 'Yangon, Bagan, Mandalay, Kalaw'
                 }
                 , {
-                    name: "China",
+                    name: 'China',
                     lat: 35.86166,
                     lon: 104.19540,
-                    description: "Hong Kong, Macau, Guilin, Changsha, Shanghai, Beijing, Xishuangbanna" +
-                                 " Kunming, Dali, Lijiang, ShangriLa, Chengdu, Chongqing"
+                    description: 'Hong Kong, Macau, Guilin, Changsha, Shanghai, Beijing, Xishuangbanna' +
+                                 ' Kunming, Dali, Lijiang, ShangriLa, Chengdu, Chongqing'
                 }
                 , {
-                    name: "Tibet",
+                    name: 'Tibet',
                     lat: 29.64692,
                     lon: 91.11721,
-                    description: "Lhasa, Shigatse, EBC, Namtso Lake"
+                    description: 'Lhasa, Shigatse, EBC, Namtso Lake'
                 }
                 , {
-                    name: "South Korea",
+                    name: 'South Korea',
                     lat: 35.90776,
                     lon: 127.76692,
-                    description: "Seoul, DMZ zone, Jeju island"
+                    description: 'Seoul, DMZ zone, Jeju island'
                 }
                 ]
             }, {
-            name: "North America",
-            img: "america.png",
+            name: 'North America',
+            img: 'america.png',
             countries: [{ 
-                    name: "Canada",
+                    name: 'Canada',
                     lat: 56.13037,
                     lon: -106.34677,
-                    description: "Vancouver and Whistler during a weekend trip",
+                    description: 'Vancouver and Whistler during a weekend trip',
                 } 
                 , {
-                    name: "United States",
+                    name: 'United States',
                     lat: 37.09024,
                     lon: -95.71289,
-                    description: "Seattle, Kent and all areas around. Portland, San Francisco and" +
-                                 " Pacific Route number 1 to Los Angeles. Napa valley, Yosemite national" +
-                                 " park. New York.",
+                    description: 'Seattle, Kent and all areas around. Portland, San Francisco and' +
+                                 ' Pacific Route number 1 to Los Angeles. Napa valley, Yosemite national' +
+                                 ' park. New York.',
                 }
             ]}
 
